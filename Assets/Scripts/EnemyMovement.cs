c7c93aee5a5f604ac8bc736f67dc32aee51a90c2
Leonardo Follador <066477@aluno.uricer.edu.br>
2025-09-16 17:53:17 -0300

    Adicionado animacao de movimento para inimigo 1

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,19 +2,24 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-
     public Rigidbody2D rb;
     public float moveSpeed;
     private Transform target;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator anim;
+
     void Start()
     {
         target = FindAnyObjectByType<PlayerMovement1>().transform;
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = (target.position - transform.position).normalized * moveSpeed;
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        rb.linearVelocity = direction * moveSpeed;
+
+        anim.SetFloat("moveX", direction.x);
+        anim.SetFloat("moveY", direction.y);
     }
 }

3ca23e6da47baf1de63901893617d292bbc337cd
Leonardo Pertile Follador <leopfollador@gmail.com>
2025-09-18 10:38:42 -0300

    feat: Melhoria no spawn e despawn da arma

--- Assets/Scripts/EnemyDamager.cs
+++ Assets/Scripts/EnemyDamager.cs
@@ -4,18 +4,33 @@ public class EnemyDamager : MonoBehaviour
 {
     public float damageAmount;
 
-    public float lifeTime;
+    public float lifeTime, growSpeed = 4f;
+    private Vector3 targetSize;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Destroy(gameObject, lifeTime);
+        //Destroy(gameObject, lifeTime);
+
+        targetSize = transform.localScale;
+        transform.localScale = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale = Vector3.MoveTowards(transform.localScale, targetSize, growSpeed * Time.deltaTime);
+    
+        lifeTime -= Time.deltaTime;
+
+        if(lifeTime <= 0)
+        {
+            targetSize = Vector3.zero;
+            if (transform.localScale.x == 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)

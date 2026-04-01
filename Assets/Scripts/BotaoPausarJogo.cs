5affe028299b9eec39467a1f390710f296e9eae6
LuizFelipeBuaszczyk <luizfelipebuaszczyk@hotmail.com>
2026-03-31 22:25:41 -0300

    fix: Quando entra tela de pause a tela fica mais escura

--- Assets/Scripts/BotaoPausarJogo.cs
+++ Assets/Scripts/BotaoPausarJogo.cs
@@ -6,17 +6,33 @@ public class BotaoPausarJogo : MonoBehaviour
     private bool pausado = false;
 
     public Button botaoPausarPartida;
+    public Button botaoDespausarPartida;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (botaoPausarPartida != null)
             botaoPausarPartida.onClick.AddListener(PausarJogo);
+        
+        if (botaoDespausarPartida != null)
+            botaoDespausarPartida.onClick.AddListener(DespausarJogo);
+        
+        botaoDespausarPartida.gameObject.SetActive(pausado);
     }
 
     private void PausarJogo()
     {
-        pausado = !pausado;
-        Time.timeScale = pausado ?  0f : 1f;
+        pausado = true;
+        Time.timeScale = 0f;
+        botaoDespausarPartida.gameObject.SetActive(pausado);
+        botaoPausarPartida.gameObject.SetActive(!pausado);
+    }
+
+    private void DespausarJogo()
+    {
+        pausado = false;
+        Time.timeScale = 1f; 
+        botaoDespausarPartida.gameObject.SetActive(pausado);
+        botaoPausarPartida.gameObject.SetActive(!pausado);
     }
 }
